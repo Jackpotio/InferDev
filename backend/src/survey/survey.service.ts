cfcee6391ab180f13806d424f238ff48c81d9de3
박재규 <101984646+MangoBoxx@users.noreply.github.com>
2026-01-25 04:08:33 +0000

    feat: 설문 결과 계산을 위한 SurveyService 구현

--- backend/src/survey/survey.service.ts
+++ backend/src/survey/survey.service.ts
@@ -1,14 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { SubmitSurveyDto } from './dto/submit-survey.dto';
+import { maxDate } from 'class-validator';
 
 @Injectable()
 export class SurveyService {
   submitSurvey(dto: SubmitSurveyDto) {
     console.log('설문 결과 수신: ', dto);
+    const { scores } = dto;
+    const result = this.calculateResult(scores);
 
     return {
       success: true,
-      message: '설문이 정상적으로 접수되었습니다.',
+      result,
     };
+  }
+
+  /*ScoreDto 기반 설문 결과 계산 */
+  private calculateResult(scores: {
+    frontend: number;
+    backend: number;
+    ai: number;
+  }): 'frontend' | 'backend' | 'ai' {
+    let maxScore = -Infinity;
+    let selectedField: 'frontend' | 'backend' | 'ai' = 'frontend';
+
+    if (scores.frontend > maxScore) {
+      maxScore = scores.frontend;
+      selectedField = 'frontend';
+    }
+
+    if (scores.backend > maxScore) {
+      maxScore = scores.backend;
+      selectedField = 'backend';
+    }
+
+    if (scores.ai > maxScore) {
+      maxScore = scores.ai;
+      selectedField = 'ai';
+    }
+    return selectedField;
   }
 }
